4fdf276bb51f79418f2e93d7fc1d24b0a9704528
cervantesac <cervantesac@SI2-17.uninorteacad.uninorte.edu.co>
2018-09-22 11:27:33 -0500

    First Commit

---
+++ src/co/edu/uninorte/prueba/Principal.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package co.edu.uninorte.prueba;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * @author cervantesac
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		Persona persona1 = new Persona("César Augusto", "Cervantes Camargo");
+		Persona persona2 = new Persona("Jhonatan Alberto", "Consuegra Lara");
+		Persona persona3 = new Persona("William Alberto", "Pareja Navarro");
+		Persona persona4 = new Persona("Victor Manuel", "Peralta Martinez");
+		
+		LinkedList<Persona> listPersonas = new LinkedList<Persona>();
+		listPersonas.add(persona1);
+		listPersonas.add(persona2);
+		listPersonas.add(persona3);
+		listPersonas.add(persona4);
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("Listado de personas - Ingresar Información");
+		System.out.println("-----------------------------------------------------------");	
+		Scanner sc = new Scanner(System.in);
+		for (Persona persona : listPersonas) {
+			System.out.println(persona.getNombreCompleto());
+			System.out.print("Digite dirección: ");
+			String direccion = sc.nextLine();
+			persona.setDireccion(direccion);
+			System.out.print("Digite teléfono: ");
+			String telefono = sc.nextLine();
+			persona.setTelefono(telefono);
+			System.out.print("Digite correo: ");
+			String correo = sc.nextLine();
+			persona.setCorreo(correo);			
+			
+		}		
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("Listado de personas");
+		System.out.println("-----------------------------------------------------------");	
+		for (Persona persona : listPersonas) {
+			System.out.println(persona.getNombreCompleto()+" "+persona.getDireccion()			
+			+" "+persona.getTelefono()
+			+" "+persona.getCorreo());		
+			
+		}		
+		System.out.println("-----------------------------------------------------------");
+		System.out.println("-----------------------------------------------------------");
+		System.out.println(persona1.getNombreCompleto()+" "+persona1.getDireccion()+" "+persona1.getTelefono()+" "+persona1.getCorreo());	
+		System.out.println(persona2.getNombreCompleto()+" "+persona2.getDireccion()+" "+persona2.getTelefono()+" "+persona2.getCorreo());		
+		System.out.println(persona3.getNombreCompleto()+" "+persona3.getDireccion()+" "+persona3.getTelefono()+" "+persona3.getCorreo());		
+		System.out.println(persona4.getNombreCompleto()+" "+persona4.getDireccion()+" "+persona4.getTelefono()+" "+persona4.getCorreo());		
+
+	}
+
+}
