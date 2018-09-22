e0a12125eb50ec44821db74f5f2520ea7be36484
cervantesac <cervantesac@SI2-17.uninorteacad.uninorte.edu.co>
2018-09-22 11:39:41 -0500

    Se adiciona los programas a la calse estudiante

--- src/co/edu/uninorte/prueba/Estudiante.java
+++ src/co/edu/uninorte/prueba/Estudiante.java
@@ -3,10 +3,23 @@
  */
 package co.edu.uninorte.prueba;
 
+import java.util.LinkedList;
+
 /**
  * @author cervantesac
  *
  */
 public class Estudiante extends Persona {
+	LinkedList<String> programas;
+	
+	public Estudiante(String nombre,String appelido) {
+		super(nombre, appelido);
+		this.programas = new LinkedList<String>();
+	}
 
+	public void setProgramas(LinkedList<String> programas) {
+		this.programas = programas;
+	}
+
+	
 }
